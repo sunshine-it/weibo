27c0125fec20fe8cb113a992f39a5545c0c083ad
vagrant <vagrant@homestead>
2019-06-20 04:29:00 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-<h1> home 页</h1>
+  <div class="jumbotron">
+    <h1>Hello Weibo</h1>
+    <p class="lead">
+      你现在所看到的是 <a href="https://weibo-2019-06-18.herokuapp.com">Weibo PC</a> 的示例项目主页。
+    </p>
+    <p>
+      一切，将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
