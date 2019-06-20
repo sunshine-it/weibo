2ff068b453ef3983bf34f482e37ae1220dfbdfc4
vagrant <vagrant@homestead>
2019-06-20 05:09:06 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer"><img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
     <a href="https://weibo-2019-06-18.herokuapp.com" target="_blank">刻意练习，每日精进</a>
 <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{ route('about') }}">关于</a>
 </div>
 </footer>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>@yield('title', 'weibo')</title><!-- <link rel="stylesheet" href="/css/app.css"> -->
+<title>@yield('title', 'weibo')</title>
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 @include('layouts._header')
 <div class="container">
-      <div class="offset-md-1 col-md-10">
+    <div class="offset-md-1 col-md-10">
         @yield('content')
         @include('layouts._footer')
-      </div>
+    </div>
 </div>
 </body>
 </html>
