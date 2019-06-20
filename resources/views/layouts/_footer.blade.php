6cb85eefcc657c736551dc46e4a4f122f5a52e7a
vagrant <vagrant@homestead>
2019-06-20 04:53:40 +0000

    fix

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer"><img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
-    <a href="https://learnku.com/laravel/courses" target="_blank">刻意练习，每日精进</a>
+    <a href="https://weibo-2019-06-18.herokuapp.com" target="_blank">刻意练习，每日精进</a>
 <div class="float-right">
     <a href="/about">关于</a>
 </div>
