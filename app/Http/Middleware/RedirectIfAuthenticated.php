11c74a8cf8671713c6d0c006d4cf985a4dd34cb9
vagrant <vagrant@homestead>
2019-06-23 04:25:13 +0000

    fix signup and login policy

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            session()->flash('info', '您已登录，无需再次操作。');
+            $msg = $request->is('signup') ? '您已注册并已登录，无需再次操作。' : '您已登录，无需再次操作。';
+            session()->flash('info', $msg);
             return redirect('/');
         }
 
