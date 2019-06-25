464fd3d2b196e58545274d9f663aa1e3e2c8fa3c
vagrant <vagrant@homestead>
2019-06-25 08:55:58 +0000

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -31,4 +31,9 @@ class UserPolicy
         // 只有当前用户拥有管理员权限且删除的用户不是自己时才显示链接
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    // 自己不能关注自己
+    public function follow(User $currentUser, User $user) {
+        return $currentUser->id !== $user->id;
+    }
 }
