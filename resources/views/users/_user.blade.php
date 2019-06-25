9fe5f9b6ebe59ff564a680e988fb1807a0b0025c
vagrant <vagrant@homestead>
2019-06-25 04:52:05 +0000

    fix

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -2,7 +2,7 @@
     <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
     <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
     @can('destroy', $user)
-    <form class="float-right" action="{{ route('users.destroy', $user->id) }}" method="POST">
+    <form class="float-right" action="{{ route('users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('您确定要删除该用户吗？');">
         {{ csrf_field() }}
         {{ method_field('DELETE') }}
         <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
