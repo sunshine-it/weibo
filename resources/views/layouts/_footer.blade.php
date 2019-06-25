26a2a4b0fe9d8b90e92736b340b4554ed958fec8
vagrant <vagrant@homestead>
2019-06-25 12:03:17 +0000

    logo

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,4 +1,5 @@
-<footer class="footer"><img class="brand-icon" src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+<footer class="footer">
+    <span style="color: #e27575;font-size: 14px;">❤</span>
     <a href="https://weibo-2019-06-18.herokuapp.com" target="_blank">学以致用，每日精进</a>
 <div class="float-right">
     <a href="{{ route('about') }}">关于</a>
